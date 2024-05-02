f4ef1c63e99e3c338502740bc7485e9183fbe613
Xigwa <bonya377@gmail.com>
2024-05-02 18:45:02 +0200

    ᅠᅠ

---
+++ src/lesson4.java
@@ -0,0 +1,11 @@
+public class lesson4 {
+    public static void main(String[] args) {
+        for (int i = 20; i > 10; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("end");
+
+    }
+
+
+}
